4487110f277fd90498f6f9fed84c6744463d635e
eric minick <eric.d.minick@gmail.com>
2023-11-21 17:18:07 -0500

    the menus to test

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pasueMenu;
+    public static bool isPaused;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pasueMenu.SetActive(false);
+    }
+    public void LeaveGame()
+    {
+        isPaused = false;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    public void PauseGame()
+    {
+        pasueMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pasueMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+        isPaused = false;
+    }
+
+    
+}
