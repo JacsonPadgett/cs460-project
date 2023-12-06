e3a2162e67376df9c87e4d42a590dac15369eac0
diasvtumane <83052154+diasvtumane@users.noreply.github.com>
2023-12-06 00:07:45 -0500

    updated subtitles

--- Assets/SubtitleManager.cs
+++ Assets/SubtitleManager.cs
@@ -8,9 +8,12 @@ public class SubtitleManager : MonoBehaviour
     public TextMeshPro subtitleText;
     [SerializeField] float timeBetweenChars;
     [SerializeField] float timeBetweenWords;
-    [SerializeField] float displayDuration; // Время отображения субтитров
-    [SerializeField] float startDelay; // Задержка перед началом субтитров
-
+    [SerializeField] float displayDuration;
+    [SerializeField] float startDelay;
+    [SerializeField] float distance = 2f;
+    [SerializeField] float height = 1f;
+    [SerializeField] float offset = 3f;
+    
     [SerializeField]
     public string[] stringArray;
 
@@ -32,7 +35,6 @@ public class SubtitleManager : MonoBehaviour
             playerHead = Camera.main.transform;
         }
 
-        // Начинаем проигрывание субтитров с задержкой
         StartCoroutine(StartSubtitles());
     }
 
@@ -47,75 +49,50 @@ public class SubtitleManager : MonoBehaviour
     {
         while (subtitleIndex < stringArray.Length)
         {
-            // Отображаем текущий субтитр
             subtitleText.text = stringArray[subtitleIndex].Substring(0, charIndex);
-
-            // Увеличиваем индекс символа для следующего кадра
             charIndex++;
 
-            // Ждем перед отображением следующего символа
             yield return new WaitForSeconds(timeBetweenChars);
 
-            // Если мы отобразили все символы в текущем субтитре, переходим к следующему
             if (charIndex > stringArray[subtitleIndex].Length)
             {
                 subtitleIndex++;
                 charIndex = 0;
 
-                // Ждем перед отображением следующего субтитра
                 yield return new WaitForSeconds(timeBetweenWords);
 
-                // Сбрасываем таймер отображения при переходе к следующему субтитру
                 displayTimer = 0f;
                 subtitlesVisible = true;
             }
         }
 
-        // Субтитры закончились, ожидаем перед скрытием
         yield return new WaitForSeconds(displayDuration);
         HideSubtitles();
     }
 
-    void Update()
+    void LateUpdate()
     {
-        // Если субтитры начались, обновляем таймер отображения
         if (subtitlesStarted)
         {
             UpdateSubtitlePositionAndRotation();
-
-            // Если субтитры отображаются, увеличиваем таймер отображения
-            if (subtitlesVisible)
-            {
-                displayTimer += Time.deltaTime;
-
-                // Если таймер превысил время отображения, скрываем субтитры
-                if (displayTimer > displayDuration)
-                {
-                    HideSubtitles();
-                }
-            }
         }
     }
 
     void UpdateSubtitlePositionAndRotation()
     {
-        // Позиция субтитров перед глазами игрока
-        Vector3 subtitlePosition = playerHead.position + playerHead.forward * 2f + Vector3.up * 2f + playerHead.right * 1f;
+        float interpolationSpeed = 5f;
 
-        // Устанавливаем позицию субтитров
-        transform.position = Vector3.Lerp(transform.position, subtitlePosition, Time.deltaTime * 5f);
+        Vector3 subtitlePosition = playerHead.position + playerHead.forward * distance + Vector3.up * height + playerHead.right * offset;
+        transform.position = Vector3.Lerp(transform.position, subtitlePosition, Time.deltaTime * interpolationSpeed);
 
-        // Поворот субтитров в сторону игрока
         Vector3 lookAtPosition = playerHead.position + playerHead.forward * 1000f;
         transform.LookAt(lookAtPosition);
 
-        // Установка фиксированного размера субтитров
         subtitleText.fontSize = 8; 
     }
 
     void HideSubtitles()
     {
-        // Скрываем субтитры
         subtitleText.text = "";
         subtitlesVisible = false;
     }
